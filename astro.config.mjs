dbbaedf068d197cfe90e3df7486a413b235008de
Juan Nuñez <juan_nunez.acid@skyairline.com>
2024-06-18 21:01:17 -0300

    Add initial project files and GitHub Actions workflow

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+    site: 'https://github.com/Juaandress/muitu24hs', 
+    base: '/muitu24hs/', 
+    build: {
+        outDir: 'dist',
+    },
+});
